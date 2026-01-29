2aee8fe870f6266d1e23e01f225fe36eefc7e272
AncheJeez <andresjsangon@gmail.com>
2026-01-29 11:55:36 +0100

    creado fichero con isnerts para la bd

---
+++ demo/INSERT_DATABASE.sql
@@ -0,0 +1,16 @@
+-- Script para insertar datos de prueba en la tabla empresa
+-- Ejecutar después de crear la base de datos y las tablas
+
+INSERT INTO empresa (nombre, nif) VALUES
+('Acme Corporation', 'A12345678'),
+('Tech Solutions S.L.', 'B87654321'),
+('Global Industries Inc', 'C11223344'),
+('Innovatech Enterprise', 'D55667788'),
+('Digital Services Group', 'E99887766'),
+('Prime Consulting Ltd', 'F44332211'),
+('Summit Manufacturing', 'G12121212'),
+('Nexus Technologies', 'H34343434'),
+('Elite Business Partners', 'I56565656'),
+('Vertex Commerce Systems', 'J78787878'),
+('Aurora Development Corp', 'K90909090'),
+('Quantum Solutions Ltd', 'L11111111');
